9d9efbe487950e0620c3139639149c4c2a607f35
Hossein Saeidian <hosseinsaeidian88@gmail.com>
2024-04-19 13:38:37 +0330

    Set validation Register

---
+++ Application/Validation/Register/RegisterRules.cs
@@ -0,0 +1,13 @@
+using atmservice.Domain.Entity;
+using FluentValidation;
+
+namespace atmservice.Application.Validation.Register
+{
+    public class RegisterRules : AbstractValidator<Expert>
+    {
+        public RegisterRules()
+        {
+            RuleFor(a => a.FullName.).NotNull();
+        }
+    }
+}
